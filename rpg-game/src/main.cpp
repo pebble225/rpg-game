af587ca91f4a94707970036e94ec6132a440b999
Caleb Bradley <dev.pebble225@gmail.com>
2021-02-25 14:10:07 -0800

    Got window up and runnning.

--- rpg-game/src/main.cpp
+++ rpg-game/src/main.cpp
@@ -5,9 +5,37 @@ SDL_Window* window;
 SDL_Surface* buffer;
 SDL_Renderer* g;
 
-int main()
+bool running = true;
+
+void input()
 {
+	SDL_Event e;
+
+	while (SDL_PollEvent(&e))
+	{
+		if (e.type == SDL_QUIT)
+		{
+			running = false;
+		}
+	}
+}
+
+int main(int argc, char* args[])
+{
+	window = SDL_CreateWindow("Window Application",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		1600, 900,
+		SDL_WINDOW_SHOWN);
+
+	buffer = SDL_GetWindowSurface(window);
+
+	g = SDL_CreateRenderer(window, -1, 0);
 	
+	while (running)
+	{
+		input();
+	}
 
 	return 0;
 }
