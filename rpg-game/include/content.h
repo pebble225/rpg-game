0cf84863eb0945529af1d3f0ec3b7963744b622b
Caleb Bradley <dev.pebble225@gmail.com>
2021-02-28 22:37:47 -0800

    Added some notes into content.h

--- rpg-game/include/content.h
+++ rpg-game/include/content.h
@@ -1,13 +1,21 @@
 #pragma once
+#include <vector>
 
 /*
 
-Generate a 65536x65536 terrain with some form of
-perlin noise.
+Generate a 65536x65536 terrain with some form of perlin noise.
+
+I need artwork whether I draw it myself or not. Characters, grass, trees, water, etc.
+
+How do I organize my constant data? Block ids, items ids, entity ids, etc.
+
+Will I regret the game's style choice eventually? 3D? Sprite sheets?
 
 */
 
+//class GameObject;
+
 class content
 {
-	
+	//vector<GameObject*> instance;
 };
